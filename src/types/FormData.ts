6ef46766e1b4fabdc8bc3517d3965c738ca80f28
Sam Hillier <sam@hillier.uk>
2025-02-10 13:58:19 +0000

    refactor: componentise motor finance form

--- src/types/FormData.ts
+++ src/types/FormData.ts
@@ -2,8 +2,8 @@ export interface FormData {
   multipleAgreements: string;
   financeProvider: string;
   policyNumberKnown: string;
-  policyNumber: string;
+  policyNumber?: string;
   vehicleNumber: string;
-  dealerOrBroker: string;
-  purchaseDate: string;
+  dealerOrBroker?: string;
+  purchaseDate?: string;
 }
